8bf74e0d9ecd4858c24d7dc239377fa7c26bb07b
Mohammed Anas <mohammedanasdk963@gmail.com>
2024-10-14 11:27:54 +0530

    implemented signup signin features

--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,5 +1,6 @@
 import { Hono } from "hono"
 import { getPrisma } from "../../db/prismaFunction"
+import { decode, verify, sign } from "hono/jwt"
 
 const user = new Hono<{
   Bindings: {
@@ -12,11 +13,115 @@ const user = new Hono<{
 }>()
 
 user.post("/signup", async (c) => {
-  return c.text("Signup")
+  // create jwt token
+  // zod validation
+  // password hashing
+
+  const body = await c.req.json()
+
+  const { username, password, firstName, lastName, email } = body
+
+  const prisma = getPrisma(c.env.DATABASE_URL)
+
+  try {
+    const user = await prisma.user.findUnique({
+      where: {
+        username,
+        email,
+      },
+    })
+
+    console.log(typeof user, "User")
+  } catch (error) {
+    console.log(error)
+  }
+
+  if (!user) {
+    c.status(409)
+    return c.json({
+      message: "User already exists",
+      statusCode: 409,
+    })
+  }
+
+  try {
+    const user = await prisma.user.create({
+      data: {
+        username,
+        password,
+        firstName,
+        lastName,
+        email,
+      },
+    })
+
+    const payload = {
+      sub: user.id,
+      role: "user",
+      exp: Math.floor(Date.now() / 1000) + 60 * 5,
+    }
+
+    const token = await sign(payload, c.env.JWT_SECRET)
+
+    c.status(200)
+
+    return c.json({
+      message: "User created successfully",
+      statusCode: 200,
+      jwt: token,
+    })
+  } catch (error) {
+    console.log(error)
+    return c.json({
+      message: "Error while creating user",
+    })
+  }
 })
 
-user.post("/signin", (c) => {
-  return c.text("signin")
+user.post("/signin", async (c) => {
+  const body = await c.req.json()
+
+  const { username, password } = body
+
+  const prisma = getPrisma(c.env.DATABASE_URL)
+
+  const user = await prisma.user.findUnique({
+    where: {
+      username,
+    },
+  })
+
+  if (!user) {
+    c.status(404)
+
+    return c.json({
+      message: "User not found",
+      statusCode: "404",
+    })
+  }
+
+  if (user.password === password) {
+    const payload = {
+      sub: user.id,
+      role: "user",
+      exp: Math.floor(Date.now() / 1000) + 60 * 5,
+    }
+
+    const token = await sign(payload, c.env.JWT_SECRET)
+
+    c.status(200)
+
+    return c.json({
+      message: "User signed in successfully",
+      statusCode: 200,
+      jwt: token,
+    })
+  } else {
+    c.status(411)
+    return c.json({
+      message: "Invalid username or password",
+    })
+  }
 })
 
 user.put("/", (c) => {
